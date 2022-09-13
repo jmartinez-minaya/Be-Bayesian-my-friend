800bf3013cb722e056fe53970379df5aa574f81c
Joaquin <jomarmina@gmail.com>
2022-09-13 18:11:55 +0200

    correcting errors

--- PRACTICAL-LESSON/besag.R
+++ PRACTICAL-LESSON/besag.R
@@ -44,7 +44,7 @@ SMR_raw_disc = cut(london.gen$SMR_raw,
 
 london.gen$SMR_raw_disc <- SMR_raw_disc
 
-png("images/SMR_raw.png", width = 1000, height = 800, res = 150)
+#png("images/SMR_raw.png", width = 1000, height = 800, res = 150)
 spplot(london.gen,
        c("SMR_raw_disc"),
        col.regions = brewer.pal(9,'Blues')[c(2,4,6,8)],
@@ -52,13 +52,13 @@ spplot(london.gen,
        par.settings =
          list(axis.line = list(col =  'transparent')))
 
-dev.off()
+#dev.off()
 
 # Covariates
 london.gen$x1 <- data$x1
 london.gen$x2 <- data$x2
 
-png("images/covs.png", width = 1200, height = 800, res = 150)
+#png("images/covs.png", width = 1200, height = 800, res = 150)
 grid.arrange(
   spplot(london.gen, c("x1"),
          main = c("index of social deprivation"),
@@ -78,7 +78,7 @@ grid.arrange(
            list(axis.line = list(col =  'transparent',
                                  legend.ticks = 'black'))),
   ncol = 2)
-dev.off()
+#dev.off()
 
 
 ### --- 3. Defining neighbor relation --- ####
@@ -167,7 +167,7 @@ mod1 <- bugs(data       = data_bugs,
 
 ### ----- 4.8. Posterior distributions of the fixed effects --- ####
 mod1_mcmc <- read.bugs(mod1)
-saveRDS(mod1_mcmc, "rds/model1_smr.RDS")
+#saveRDS(mod1_mcmc, "rds/model1_smr.RDS")
 mod1_mcmc <- readRDS("rds/model1_smr.RDS")
 summary(mod1_mcmc)[[2]] %>% round(., 3)%>% .[1:2,]
 
@@ -323,135 +323,13 @@ dev.off()
 
 
 
-
-
-
-### --- 5. Fitting the model using INLA bym effect --- ####
-#This create a file called ``LDN.graph'' with the graph for INLA
-nb2INLA("data/processed/london_suic/LDN.graph", temp)
-
-### ----- 5.1. Plotting the generated graph --- ####
-H <- inla.read.graph(filename="data/processed/london_suic/LDN.graph")
-image(inla.graph2matrix(H),xlab="",ylab="")
-
-### ----- 5.2. Adding ids for the random effects --- ####
-S <- U <- seq(1,32)
-data <- cbind(data, S, U)
-
-
-### ----- 5.3. Formula --- ####
-formula <- y ~ 1 + f(S, 
-                     model       = "besag", 
-                     graph       = H,
-                     scale.model = TRUE,
-                     hyper       = 
-                       list(prec = list(prior="loggamma",param = c(1,0.001)))) +
-  f(U, 
-    model       = "iid",
-    hyper       = 
-      list(prec = list(prior="loggamma",param = c(1,0.001))))
-
-### ----- 5.4. Model --- ####
-mod.suicides <- inla(formula,
-                     family          = "poisson",
-                     data            = data,
-                     E               = E,
-                     control.compute = list(dic = TRUE, waic = TRUE, cpo = TRUE),
-                     control.predictor = list(compute=TRUE, cdf=c(log(1))))
-
-summary(mod.suicides)
-
-
-### ----- 5.5. Posterior distribution of the random effects --- ####
-london.gen$SPmean <- round(mod.suicides$summary.random$S[["mean"]], 4)
-london.gen$SPsd <- round(mod.suicides$summary.random$S[["sd"]],5)
-
-#png("images/random_effect_spatial_bym.png", width = 1500, height = 700, 
-#    res = 100)
-grid.arrange(
-  spplot(london.gen, c("SPmean"),
-         main = c("Mean posterior of S"),
-         #col.regions = rev(viridis_pal(option = "B")(101)),
-         col.regions = colorRampPalette(brewer.pal(9,'Blues'))(101),
-         cuts        = 100,
-         colorkey=list(space="bottom", space = "bottom"),
-         par.settings =
-           list(axis.line = list(col =  'transparent',
-                                 legend.ticks = 'black'))),
-  spplot(london.gen, c("SPsd"),
-         main = c("Sd posterior of S"),
-         col.regions = colorRampPalette(brewer.pal(9,'Blues'))(101),
-         cuts        = 100,
-         colorkey=list(space="bottom", space = "bottom"),
-         par.settings =
-           list(axis.line = list(col =  'transparent',
-                                 legend.ticks = 'black'))),
-  ncol = 2)
-#dev.off()
-
-### ----- 5.6. Posterior distribution of suicides mortality --- ####
-london.gen$SMR_mean <- mod.suicides$summary.fitted.values$mean # mean
-london.gen$SMR_sd <- mod.suicides$summary.fitted.values$sd #s
-london.gen$SMR_median <- mod.suicides$summary.fitted.values$`0.5quant` # median
-london.gen$SMR_q025 <- mod.suicides$summary.fitted.values$`0.025quant` # quantile
-london.gen$SMR_q975 <- mod.suicides$summary.fitted.values$`0.975quant` # quantile
-london.gen$SMR_p1 <- 1 - mod.suicides$summary.fitted.values$`1 cdf` # probability to be greater than 1
-
-#png("images/SMR_bym.png", width = 1500, height = 600, 
-#    res = 100)
-grid.arrange(spplot(london.gen,
-                    c("SMR_mean"),
-                    col.regions = colorRampPalette(brewer.pal(9,'Blues'))(101),
-                    cuts         = 100,
-                    main        = "SMR mean ",
-                    colorkey=list(space="bottom"),
-                    par.settings =
-                      list(axis.line = list(col =  'transparent'))),
-             spplot(london.gen,
-                    c("SMR_sd"),
-                    col.regions = colorRampPalette(brewer.pal(9,'Blues'))(101),
-                    cuts         = 100,
-                    main        = "SMR sd ",
-                    colorkey=list(space="bottom"),
-                    par.settings =
-                      list(axis.line = list(col =  'transparent'))), ncol = 2)
-
-#dev.off()
-
-
-### ----- 5.7. Posterior distribution of suicides SMR with cutoff--- ####
-## Also, the probability for SMR to be greater than 1.
-SMR.cutoff<- c(0.6, 0.9, 1.0, 1.1,  1.8)
-SMR_p1.cutoff <- c(0,0.2,0.8,1)
-
-SMR_disc = cut(london.gen$SMR_mean,
-               breaks         = SMR.cutoff,
-               include.lowest = TRUE)
-
-SMR_p1_disc = cut(london.gen$SMR_p1,
-                  breaks         = SMR_p1.cutoff,
-                  include.lowest = TRUE)
-
-
-london.gen$SMR_disc <- SMR_disc
-london.gen$SMR_p1_disc <- SMR_p1_disc
-
-#png("images/SMR_bym_disc.png", width = 1500, height = 600, res = 150)
-grid.arrange(spplot(london.gen,
-                    c("SMR_disc"),
-                    col.regions = brewer.pal(9,'Blues')[c(2,4,6,8)],
-                    main        = "SMR ",
-                    par.settings =
-                      list(axis.line = list(col =  'transparent'))),
-             spplot(london.gen,
-                    c("SMR_p1_disc"),
-                    col.regions = brewer.pal(9,'Blues')[c(3,6,9)],
-                    main        = "p(SMR > 1) ",
-                    par.settings =
-                      list(axis.line = list(col =  'transparent'))), ncol = 2)
-
 #dev.off()
 
 ### --- 5. Excercise --- ###
-#Now, add the covariates (deprivation - x1 and social fragmentation - x2) and 
+#Now, add the covariate X, social fragmentation and 
 #repeat the steps
+
+# Add an independent random effect
+
+
+
